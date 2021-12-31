8d148dd1bfb1ed341a0186729b40eb2a65aac868
atiye <atiyenaderi81@gmail.com>
2021-12-31 20:24:01 +0330

    push

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import {Button} from "@material-ui/core";
 export default function Home() {
     const history = useHistory();
     return (
-        <div style={{display:'flex', justifyContent:'center',alignItems:'center',width:'100%',border:'solid red', height:'100vh'}}>
+        <div style={{display:'flex', justifyContent:'center',alignItems:'center',width:'100%', height:'100vh'}}>
             <Button variant='contained'
                     style={{backgroundColor:'#b973e8', height:50, fontWeight:'bold'}}
                     onClick={() => history.push('/users')}>
